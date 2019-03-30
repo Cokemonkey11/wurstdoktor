f60bac2edaaa3412cdb1dae9b8257eebdad883db
Cokemonkey11 <Cokemonkey11@gmail.com>
2019-03-30 12:02:21 +0000

    minor doc improvements

--- publish/publish.sh
+++ publish/publish.sh
@@ -10,5 +10,6 @@ find ../../WurstStdlib2/ -name '*.wurst' | \
     head -n 100 | \
     xargs -I {} bash -c " \
         mkdir -p \$( echo '{}' | cut -d'/' -f5- | rev | cut -d'/' -f2- | rev ) && \
+        echo '{}' >&2 && \
         ../target/release/wurstdoktor.exe < {} > \$( echo '{}' | cut -d '/' -f5- ).yaml \
     "
